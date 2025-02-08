15b4537738e6bf08ac21a990df42f492467a8d4b
semilunaris <vlad.karpych@gmail.com>
2025-02-08 10:42:31 +0300

    HW9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -1,38 +1,40 @@
-import React, {useState} from 'react'
-import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
-import {restoreState} from '../hw06/localStorage/localStorage'
-import s from './Clock.module.css'
+import React, { useEffect, useState } from 'react';
+import SuperButton from '../hw04/common/c2-SuperButton/SuperButton';
+import { restoreState } from '../hw06/localStorage/localStorage';
+import s from './Clock.module.css';
 
 function Clock() {
-    const [timerId, setTimerId] = useState<number | undefined>(undefined)
-    // for autotests // не менять // можно подсунуть в локалСторэдж нужную дату, чтоб увидеть как она отображается
-    const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())))
-    const [show, setShow] = useState<boolean>(false)
+    const [timerId, setTimerId] = useState<number | undefined>(undefined);
+    const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())));
+    const [show, setShow] = useState<boolean>(false);
 
     const start = () => {
-        // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
-        // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
+        if (timerId) return;
 
-    }
+        const id = window.setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+
+        setTimerId(id);
+        console.log('Таймер запущен, ID:', id);
+    };
 
     const stop = () => {
-        // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
+        if (timerId) {
+            clearInterval(timerId);
+            setTimerId(undefined);
+            console.log('Таймер остановлен');
+        }
+    };
 
-    }
+    const onMouseEnter = () => {};
 
-    const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
+    const onMouseLeave = () => {};
 
-    }
-    const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
-
-    }
-
-    const stringTime = 'date->time' || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
-
-    // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = 'date->day' || <br/> // пишут студенты
-    const stringMonth = 'date->month' || <br/> // пишут студенты
+    const stringTime = date.toLocaleTimeString('ru-RU') || <br />;
+    const stringDate = date.toLocaleDateString('ru-RU') || <br />;
+    const stringDay = date.toLocaleDateString('ru-RU', { weekday: 'long' }) || <br />;
+    const stringMonth = date.toLocaleString('en-US', { month: 'long' }) || <br />;
 
     return (
         <div className={s.clock}>
@@ -57,7 +59,7 @@ function Clock() {
                         </>
                     ) : (
                         <>
-                            <br/>
+                            <br />
                         </>
                     )}
                 </div>
@@ -66,21 +68,21 @@ function Clock() {
             <div className={s.buttonsContainer}>
                 <SuperButton
                     id={'hw9-button-start'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер запущен
+                    disabled={!!timerId}
                     onClick={start}
                 >
                     start
                 </SuperButton>
                 <SuperButton
                     id={'hw9-button-stop'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер не запущен
+                    disabled={!timerId}
                     onClick={stop}
                 >
                     stop
                 </SuperButton>
             </div>
         </div>
-    )
+    );
 }
 
-export default Clock
+export default Clock;
